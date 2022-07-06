c6116a8576f129b3c62ecc47152a0f2b19235ec9
rice <abcd2733642897@gmail.com>
2022-07-06 11:37:14 +0800

    oooooooo

--- Assets/scricps/s1.cs
+++ Assets/scricps/s1.cs
@@ -7,7 +7,7 @@ public class s1 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug 
     }
 
     // Update is called once per frame
